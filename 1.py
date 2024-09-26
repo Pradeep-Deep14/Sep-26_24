081dc33df9cf883e17435cbb3a50dcdfdd4e61ef
Pradeep <deeppradeep1998@gmail.com>
2024-09-26 19:44:36 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,15 @@
+def func():
+    a,*b=["Harry","Precious","Peter"]
+    names=b
+    return "peter" in names
+
+print(func())
+
+
+#list unpacking
+#a="Harry"
+#b="Precious","Peter"
+#names=b
+#It will return true if Peter case is correct
+#False is the output since case is varying.
+
